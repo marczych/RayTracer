26dd4b9c05a71be37b6d98625a2fa0ed7ca4c328
Marc Zych <marczych@gmail.com>
2013-06-29 00:36:50 -0700

    Clean up Boundaries

--- src/Boundaries.h
+++ src/Boundaries.h
@@ -3,28 +3,18 @@
 
 #include <math.h>
 #include "Vector.h"
-#include "Ray.h"
-#include "Object.h"
-#include "Intersection.h"
 
 using namespace std;
 
 class Boundaries {
-
 public:
    Vector min, max;
 
-   Boundaries() :
-     min(Vector(0, 0, 0)), max(Vector(0, 0, 0)) {}
-
-   Boundaries(Vector min_, Vector max_) :
-      min(min_), max(max_) {}
-
-   Boundaries(const Boundaries& other) :
-      min(other.min), max(other.max) {}
+   Boundaries() : min(Vector(0, 0, 0)), max(Vector(0, 0, 0)) {}
+   Boundaries(Vector min_, Vector max_) : min(min_), max(max_) {}
+   Boundaries(const Boundaries& other) : min(other.min), max(other.max) {}
 
    double splitValue(char axis);
-   void split(Boundaries*, Boundaries*, char axis);
 };
 
 #endif
